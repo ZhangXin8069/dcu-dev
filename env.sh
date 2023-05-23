9b31a5ce404013023143528297c079590031bb79
ZhangXin8069 <96939039+ZhangXin8069@users.noreply.github.com>
2023-05-23 10:28:40 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,5 +1,15 @@
-# path init
+# source 
+_PATH=`cd "$(dirname "$0")";pwd` 
+source ${_PATH}/../env.sh 
+  
+# init 
 _HOME=$(pwd)
+_NAME=`basename "$0"` 
+name='test' 
+work_name="test" 
+tmp_name="tmp" 
+work_path=${_HOME}/${work_name} 
+tmp_path=${_HOME}/${tmp_name}
 
 #do
 source ${_HOME}/shell_script/scripts.sh
